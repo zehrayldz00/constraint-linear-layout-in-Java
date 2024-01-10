05a50fc5e05e821da9d9e4427d1e407ba31468bd
Zehra Gul Yildiz <zehragul15@gmail.com>
2024-01-10 23:59:47 +0300

    Relative

---
+++ ConstraintLinearRelative/app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kayit_ol);
+    }
+
+    public void btnSignIn(View v){
+
+    }
+}
